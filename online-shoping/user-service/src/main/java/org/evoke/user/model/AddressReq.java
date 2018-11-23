42d21ec86dfad15dead474e2e516100c36bf17db
vvyasabhattu <vvyasabhattu@evoketechnologies.com>
2018-11-23 12:17:10 +0530

    new model class

---
+++ online-shoping/user-service/src/main/java/org/evoke/user/model/AddressReq.java
@@ -0,0 +1,17 @@
+package org.evoke.user.model;
+
+public class AddressReq extends BaseRequest
+{
+	private Address address;
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	
+	
+}
